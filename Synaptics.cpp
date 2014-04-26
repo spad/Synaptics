f68bab91c6b8b46a67b6a65991fdb85d012af11b
Saverio <saveriop@gmail.com>
2014-04-26 14:09:35 +0200

    Start

---
+++ Synaptics.cpp
@@ -0,0 +1,367 @@
+/*
+ * ps2.cpp - an interface library for ps2 devices.  Common devices are
+ * mice, keyboard, barcode scanners etc.  See the examples for mouse and
+ * keyboard interfacing.
+ * limitations:
+ *      we do not handle parity errors.
+ *      The timing constants are hard coded from the spec. Data rate is
+ *         not impressive.
+ *      probably lots of room for optimization.
+ */
+
+#include "Synaptics.h"
+#include "Arduino.h"
+/*
+ * the clock and data pins can be wired directly to the clk and data pins
+ * of the Ps2 connector.  No external parts are needed.
+ */
+Synaptics::Synaptics(int clk, int data)
+{
+	_ps2clk = clk;
+	_ps2data = data;
+
+	gohi(_ps2clk);
+	gohi(_ps2data);
+	disable();
+	set_remote_mode();
+}
+
+void
+Synaptics::gohi(int pin)
+{
+	pinMode(pin, INPUT);
+	digitalWrite(pin, HIGH);
+}
+
+void
+Synaptics::golo(int pin)
+{
+	pinMode(pin, OUTPUT);
+	digitalWrite(pin, LOW);
+}
+
+/*
+Perform a software reset and recalibration as described in section
+3.3. Response is ACK ($FA), followed by $AA, $00 after a
+calibration delay of 300–500ms
+*/
+bool
+Synaptics::reset(void)
+{
+	uint8_t success;
+	write(0xff); 		// send reset command
+	read_ack();			// read ack
+	delay(500);			// wait for calibration
+	success = read();	// device response: 0xaa = success; 0xfc = failure
+	read();				// read device id. Always 0x00
+	return (success==0xaa);
+}
+
+/* Switch to Remote mode, as distinct from the default
+Stream mode. In Remote mode, the device sends motion data packets only
+in response to a Read Data ($EB) command */
+void
+Synaptics::set_remote_mode(void)
+{
+	write(0xf0); 	// send command "Set Remote mode"
+	read_ack(); 	// read ack
+	_mode = PAD_REMOTE_MODE;
+}
+
+void
+Synaptics::disable(void) 
+{
+	write(0xf5); 	// send command "disable"
+	read_ack(); 	// read ack
+	_status = PAD_STATUS_DISABLED;
+}
+
+void
+Synaptics::enable(void) 
+{
+	write(0xf4); 	// send command "enable"
+	read_ack(); 	// read ack
+	_status = PAD_STATUS_ENABLED;
+}
+
+/*
+Response is an ACK ($FA), followed by a 3-byte status
+packet consisting of the following data
+
+		bit7	bit6	bit5	bit4	bit3	bit2	bit1 	bit0
+____________________________________________________________________
+byte1|	 0     Remote  Enable  Scaling    0     Left   Middle  Right
+byte2|	 0        0       0       0       0       0      Resolution
+byte3|                           Sample Rate
+
+Remote: 1 = Remote (polled) mode, 0 = Stream mode.
+Enable: 1 = Data reporting enabled, 0 = disabled. This bit only has effect in Stream mode.
+Scaling: 1 = Scaling is 2:1, 0 = scaling is 1:1. See commands $E6 and $E7 below.
+Left: 1 = Left button is currently pressed, 0 = released.
+Middle: 1 = Middle button is currently pressed, 0 = released.
+Right: 1 = Right button is currently pressed, 0 = released.
+Resolution: The current resolution setting, from 0 to 3 (1, 2, 4, and 8 counts per mm).
+Sample rate: The current sample rate setting, from 10 to 200.
+*/
+void 
+Synaptics::status_request(uint8_t data[3])
+{
+	write(0xe9);
+	read_ack();
+	data[0] = read();
+	data[1] = read();
+	data[2] = read();
+}
+
+/*
+In the default Relative format, each motion packet consists of three bytes. The first byte
+encodes various status bits, and the other two bytes encode the amount of motion in X
+and Y that has occurred since the previous packet.
+
+		bit7	bit6	bit5	bit4	bit3	bit2	bit1 	bit0
+____________________________________________________________________
+byte1| Y_ovfl  X_ovfl  Y_sign  X_sign    1     Middle   Right   Left
+byte2|	 							X delta
+byte3|                              Y delta
+
+When Absolute mode is enabled, each motion report consists of six bytes. These bytes
+encode the absolute X, Y location of the finger on the sensor pad, as well as the Z
+(pressure) value and various other measurements and status bits. Section 2.3 discusses
+the contents of the Absolute mode packet in great detail.
+*/
+void
+Synaptics::read_data(/*uint8_t data[6]*/)
+{
+	write(0xeb);
+	read_ack();
+	data[0] = read();
+	data[1] = read();
+	data[2] = read();
+}
+
+
+void
+Synaptics::special_sequence(int sequence_type, uint8_t param, uint8_t data[])
+{
+	// send initial sequence
+	write(0xe8); read_ack();
+	write((param >> 6) & 0x03); read_ack();
+	write(0xe8); read_ack();
+	write((param >> 4) & 0x03); read_ack();
+	write(0xe8); read_ack();
+	write((param >> 2) & 0x03); read_ack();
+	write(0xe8); read_ack();
+	write(param & 0x03); read_ack();
+	// send command
+	if (sequence_type == PAD_SEQ_INFO) 
+	{
+		write(0xe9);
+		read_ack();
+		// returns data 
+		data[0] = read();
+		data[1] = read();	// usually 0x47 except for 0x03 param on recent models
+		data[2] = read();
+	} else if (sequence_type == PAD_SEQ_SET_MODE)
+	{
+		write(0xf3);
+		read_ack();
+		write(0x14);
+		read_ack();
+		//no result
+	}
+}
+
+
+/*
+		    bit7	  bit6	  bit5	  bit4	  bit3	  bit2	  bit1	 bit0
+		____________________________________________________________________
+byte1|	 							   InfoMinor
+byte2|	 								 0x047
+byte3|   			infoModelCode		    | 			infoMajor
+*/
+void 
+Synaptics::identify(uint8_t data[3])
+{
+	special_sequence(PAD_SEQ_INFO,0x00, data);
+}
+
+/**
+* returns the mode byte. 
+* bit(7): 1=>absolute mode; 0=>relative mode
+* bit(6): 0=>40; 1=>80; packet per second
+* bit(3): 1=>sleep; 0=>normal;
+* bit(2): This bit is 0 to enable “tap” and “drag” gesture processing, or 1 to disable
+		  detection of tap and drag gestures. When this bit is 1, the Relative mode
+		  mouse packet reports the true physical button states, and the Absolute mode
+		  packet’s Gesture bit always reports as zero. The DisGest bit is implemented
+		  only for 40.x and later TouchPads (i.e., when infoMajor ≥ 4); for older pads,
+		  the bit is reserved.
+* bit(0): 0=>normal Absolute mode packets; 1=>enhanced Absolute packets that contain the “W”
+*/
+uint8_t 
+Synaptics::read_modes(void)
+{
+	uint8_t data[3];
+	special_sequence(PAD_SEQ_INFO,0x01, data);
+	return data[2];
+}
+
+/**
+* returns 3 bytes device capabilities.
+		    bit7	  bit6	  bit5	  bit4	  bit3	  bit2	  bit1	 		bit0
+		________________________________________________________________________
+byte1|	 cExended		-		-		-	   -		-		-	  	 	-
+byte2|	 								 0x047
+byte3|   	 -			-		-	 cSleep	cFourBtn 	-   cMultiFIng	 cPalmDet
+
+capExtended (bit 15)
+	This bit is set if the extended capability bits are supported. The host can
+	examine this bit to see whether the other 15 extended capability bits are
+	present;
+capSleep (bit 4)
+	For the PS/2 protocol, the capSleep bit is set if sleep mode is supported
+capFourButtons (bit 3)
+	For the PS/2 protocol, this bit is set if the pad is a “MultiSwitch” pad which
+	supports four mouse buttons labeled Left, Right, Up, and Down. In the
+	PS/2 protocol, the Up and Down buttons are reported only during Absolute
+	Mode with the Wmode bit set
+capMultiFinger (bit 1)
+	This bit is set if multi-finger detection is supported. The pad is then able to
+	count the number of simultaneous fingers on the pad and report the finger
+	count via the W field of the Absolute packet. If this bit is 0, the pad does
+	not support multi-finger detection; any finger contact will be assumed to be
+	a single finger, with the position reported as the midpoint between all actual
+	fingers, and, if capPalmDetect is set, with W reporting a (typically large)
+	“width” for the assumed single finger.
+capPalmDetect (bit 0)
+	This bit is set if “palm detection” is supported. In “W mode,” the TouchPad
+	measures the apparent size or width of the finger and reports the width in
+	the W field of the Absolute mode packet. The host can use this information
+	to help distinguish between intentional finger contact and accidental palm
+	or hand contact.	
+*/
+void 
+Synaptics::read_capabilities(uint8_t data[3])
+{
+	special_sequence(PAD_SEQ_INFO,0x02, data);
+}
+
+void 
+Synaptics::read_modelid(uint8_t data[3])
+{
+	special_sequence(PAD_SEQ_INFO,0x03, data);
+}
+
+/**
+* set mode for pad. See section 2.5 for details (or .h file)
+*/
+void
+Synaptics::set_mode(uint8_t mode)
+{
+	special_sequence(PAD_SEQ_SET_MODE,mode, NULL);
+}
+
+/* write a byte to the Synaptics device */
+void
+Synaptics::write(uint8_t data)
+{
+	uint8_t i;
+	uint8_t parity = 1;
+	
+	gohi(_ps2data);
+	gohi(_ps2clk);
+	delayMicroseconds(300);
+	golo(_ps2clk);
+	delayMicroseconds(300);
+	golo(_ps2data);
+	delayMicroseconds(10);
+	gohi(_ps2clk);	// start bit
+	/* wait for device to take control of clock */
+	while (digitalRead(_ps2clk) == HIGH) {;}
+	// clear to send data
+	for (i=0; i < 8; i++)
+	{
+		if (data & 0x01)
+		{ 
+			gohi(_ps2data);
+		} else {
+			golo(_ps2data);
+		}
+		// wait for clock
+		while (digitalRead(_ps2clk) == LOW) {;}
+		while (digitalRead(_ps2clk) == HIGH){;}
+		parity = parity ^ (data & 0x01);
+		data = data >> 1;
+	}
+	// parity bit
+	if (parity)
+	{
+		gohi(_ps2data);
+	} else {
+		golo(_ps2data);
+	}
+	// clock cycle - like ack.
+	while (digitalRead(_ps2clk) == LOW){;}
+	while (digitalRead(_ps2clk) == HIGH){;}
+	// stop bit
+	gohi(_ps2data);
+	delayMicroseconds(50);
+	while (digitalRead(_ps2clk) == HIGH){;}
+	// mode switch
+	while ((digitalRead(_ps2clk) == LOW) || (digitalRead(_ps2data) == LOW)){;}
+	// hold up incoming data
+	golo(_ps2clk);
+}
+
+
+/*
+ * read a byte of data from the ps2 device.  Ignores parity.
+ */
+uint8_t
+Synaptics::read(void)
+{
+	uint8_t data = 0x00;
+	uint8_t i;
+	uint8_t bit = 0x01;
+
+	// start clock
+	gohi(_ps2clk);
+	gohi(_ps2data);
+	delayMicroseconds(50);
+	while (digitalRead(_ps2clk) == HIGH)
+		;
+	delayMicroseconds(5);	// not sure why.
+	while (digitalRead(_ps2clk) == LOW)
+		;	// eat start bit
+	for (i=0; i < 8; i++)
+	{
+		while (digitalRead(_ps2clk) == HIGH)
+			;
+		if (digitalRead(_ps2data) == HIGH)
+		{
+			data = data | bit;
+		}
+		while (digitalRead(_ps2clk) == LOW)
+			;
+		bit = bit << 1;
+	}
+	// eat parity bit, ignore it.
+	while (digitalRead(_ps2clk) == HIGH)
+		;
+	while (digitalRead(_ps2clk) == LOW)
+		;
+	// eat stop bit
+	while (digitalRead(_ps2clk) == HIGH)
+		;
+	while (digitalRead(_ps2clk) == LOW)
+		;
+	golo(_ps2clk);	// hold incoming data
+
+	return data;
+}
+
+bool
+Synaptics::read_ack(void) 
+{
+	return (read()==0xfa);
+}
---
+++ Synaptics.h
@@ -0,0 +1,69 @@
+/*
+ * Synaptics.h - a library to interface with ps2 Synaptics
+ * touchpad.
+ * Written by Saverio Pieri.
+ * Release into public domain.
+ */
+#include <inttypes.h>
+
+#ifndef synaptics_h
+#define synaptics_h
+
+#define PAD_REMOTE_MODE 0x01 // only remote mode supported 
+#define PAD_STREAM_MODE 0x02 // for reference only. Not used
+
+#define PAD_STATUS_ENABLED  0x01 
+#define PAD_STATUS_DISABLED 0x02 
+
+#define PAD_SEQ_INFO  		0x01 // information queries command sequence 
+#define PAD_SEQ_SET_MODE 	0x02 // set mode command sequence
+
+#define PAD_MODE_ABSOLUTE	0x80 // bit7 1 to select Absolute mode
+#define PAD_MODE_RATE_80	0x40 // bit6 1 to select high (80) packet rate
+#define PAD_MODE_SLEEP		0x30 // bit3 1 to select sleep mode
+#define PAD_MODE_DISGEST	0x20 // bit2 1 to disable detection of tap and drag gestures
+#define PAD_MODE_PACKETSIZE 0x10 // not used in ps2 mode
+#define PAD_MODE_WMODE		0x01 // 0 to select normal Absolute mode packets, or 1 to select
+								 // enhanced Absolute packets that contain the “W” value
+#define PAD_MODE_COMMON_00	0x01 // Always OK Relative mode
+#define PAD_MODE_COMMON_04  0x04 // Version 4.x or later Relative mode with gestures disabled
+#define PAD_MODE_COMMON_40 	0x40 // Always OK Relative mode with high packet rate
+#define PAD_MODE_COMMON_80 	0x80 // capExtended = 0 Absolute mode
+#define PAD_MODE_COMMON_81 	0x81 // capExtended = 1 Absolute mode with W
+#define PAD_MODE_COMMON_C0  0xC0 // capExtended = 0 Absolute mode with high packet rate
+#define PAD_MODE_COMMON_C1  0xC1 // capExtended = 1 Absolute mode with W, high packet rate
+#define PAD_MODE_COMMON_0C  0x0C // capSleep = 1 Low-power sleep mode
+
+
+class Synaptics
+{
+	public:
+		Synaptics(int clk, int data);
+		void write(uint8_t data);
+		uint8_t read(void);
+		bool reset();
+		void disable(void);
+		void enable(void);
+		void set_remote_mode(void);
+		void status_request(uint8_t data[3]); // ...read buttons status
+		void read_data(uint8_t data[6]);
+		void special_sequence(int sequence_type, uint8_t param, uint8_t data[]);
+		// information queries
+		void identify(uint8_t data[3]);
+		uint8_t read_modes(void);
+		void read_capabilities(uint8_t data[3]);
+		void read_modelid(uint8_t data[3]);
+		// set mode sequence
+		void set_mode(uint8_t mode);
+	private:
+		int _ps2clk;
+		int _ps2data;
+		int _mode;
+		int _status;
+		void golo(int pin);
+		void gohi(int pin);
+		bool read_ack(void);
+};
+
+#endif /* synaptics_h */
+
